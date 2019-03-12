589cce88f0ac9fbc105ef8ad68b31399cbd04d4d
Tomasz Hoffmann <tomaszhoffmann0@gmail.com>
2019-03-12 17:01:07 +0100

    Zmiany

--- PackageImport/Program.cs
+++ PackageImport/Program.cs
@@ -10,26 +10,16 @@ namespace PackageImport
 {
     class Program
     {
-                static void LoadExcelMain(string excelPath)
+                static void LoadExcelMain(string excelPath, string databseConnection)
         {
 
             string conString = string.Empty;
             string extension = Path.GetExtension(excelPath);
-       
-            switch (extension)
-            {
-                case ".xls": //Excel 97-03
-                    conString = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
-                    break;
-                case ".xlsx": //Excel 07 or higher
-                    conString = ConfigurationManager.ConnectionStrings["Excel07+ConString"].ConnectionString;
-                    break;
-
-            }
-    
+            conString = ConfigurationManager.ConnectionStrings["Excel07+ConString"].ConnectionString;
             conString = string.Format(conString, excelPath);
             using (OleDbConnection excel_con = new OleDbConnection(conString))
             {
+                int lp = 0;
                 excel_con.Open();
                 //read sheet named TABLE_NAME from excell , with columns : Name , Salary
                 string sheet1 = excel_con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null).Rows[0]["TABLE_NAME"].ToString();
@@ -46,34 +36,36 @@ namespace PackageImport
                 }
                 excel_con.Close();
              
-                string consString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+                string consString = databseConnection;
                 using (SqlConnection con = new SqlConnection(consString))
                 using (SqlCommand command = con.CreateCommand())
                 {
+                    
                     con.Open();
                     command.Parameters.Add("@NR_ART", SqlDbType.NVarChar);
                     command.Parameters.Add("@opak1", SqlDbType.Int);
                     command.Parameters.Add("@opak2", SqlDbType.Int);
                     command.Parameters.Add("@opak3", SqlDbType.Int);
 
-                    command.Parameters.Add("@opisOpak1", SqlDbType.NVarChar);
+                   /* command.Parameters.Add("@opisOpak1", SqlDbType.NVarChar);
                     command.Parameters.Add("@opisOpak2", SqlDbType.NVarChar);
-                    command.Parameters.Add("@opisOpak3", SqlDbType.NVarChar);
+                    command.Parameters.Add("@opisOpak3", SqlDbType.NVarChar);*/
 
                     foreach (DataRow row in dtExcelData.Rows)
                     {
+                        lp += 1;
                         string nr_art = Convert.ToString(row["Materiał"]); //nr_art
-                        string opisOpak1 = Convert.ToString(row["Opis opak1"]);
+                       /* string opisOpak1 = Convert.ToString(row["Opis opak1"]);
                         string opisOpak2 = Convert.ToString(row["Opis opak2"]);
-                        string opisOpak3 = Convert.ToString(row["Opis opakowanie 3"]);
+                        string opisOpak3 = Convert.ToString(row["Opis opakowanie 3"]);*/
 
                         int opak1 = Convert.ToInt32(row["Ilo#opak#1"]);
                         int opak2 = Convert.ToInt32(row["Ilo#opak#2"]);
                         int opak3 = Convert.ToInt32(row["Ilo#opak#3"]);
 
+                      
 
-
-                        command.CommandText = "UPDATE DBO.NHANDLOTABLE SET opak1=@opak1, opak2=@opak2, opak3=@opak3, opisOpak1=@opisOpak1,opisOpak2=@opisOpak2,opisOpak3=@opisOpak3 WHERE NR_ART=@NR_ART";
+                        command.CommandText = "UPDATE DBO.NHANDLOTABLE SET przelicz=@opak1, PRZELSKRZ=@opak2, PRZELPALET=@opak3 WHERE NR_ART=@NR_ART";
 
                         command.Parameters["@NR_ART"].Value = nr_art;
 
@@ -81,18 +73,18 @@ namespace PackageImport
                         command.Parameters["@opak2"].Value = opak2; //skrzynka
                         command.Parameters["@opak3"].Value = opak3; //paleta
 
-                        command.Parameters["@opisOpak1"].Value = opisOpak1; 
+                        /*command.Parameters["@opisOpak1"].Value = opisOpak1; 
                         command.Parameters["@opisOpak2"].Value = opisOpak2; 
-                        command.Parameters["@opisOpak3"].Value = opisOpak3; 
+                        command.Parameters["@opisOpak3"].Value = opisOpak3; */
 
                         try
                         {
                             command.ExecuteNonQuery();
-                            Console.WriteLine(nr_art + " complete");
+                            Console.WriteLine(lp+" - "+ nr_art + " complete");
                         }
                         catch(Exception)
                         {
-                            Console.WriteLine(nr_art + " failed");
+                            Console.WriteLine(lp + " - " + nr_art + " failed");
                         }
                     }
                     con.Close();
@@ -107,7 +99,7 @@ namespace PackageImport
         
         static void Main(string[] args)
         {
-            LoadExcelMain(ConfigurationManager.ConnectionStrings["ExcelPath"].ConnectionString);
+            LoadExcelMain(@"C:\Users\tomasz.hoffmann\Desktop\pt.xlsx", @"Data Source=.\sqlexpress;Initial Catalog=Test2; Integrated Security=False;User ID=sa;Password=Whokna123@");
         }
     }
 }
